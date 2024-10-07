fbe203a0b40984792835d0ffee18b52a48c5a9a1
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-10-07 07:41:17 +0100

    Add files via upload

---
+++ Code/System/Error/Crash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackcombOS.System.Error
+{
+    internal class crash
+    {
+        public void Crash(string error)
+        {
+            Console.Clear();
+            Console.WriteLine(" @@@@@@@@@ @@@@@@@@@\r\n @@@@@@@@@@@@@@@@@@@@\r\n @@@@@@@@@@@@@@@@@@@\r\n  @@@@@@@@@@@@@@@@@  \r\n  @@@@@@@@ @@@@@@@@  \r\n @@@ @@@@@@@@@@@@@@@ \r\n @@@@@@@@@@@    @@@@ \r\n@@ @@@@@@@@@    @@ @@\r\n@@@@@@@@@@@@@@  @@@@@\r\n@@@@@@@@@    @@@@@@@@\r\n @@@  @@@    @@   @@ \r\n  @@@ @@@@@@@@@  @@@ \r\n    @@@@@@@@@@@@@    \r\n       @@@@@@@       \r\n");
+            Console.WriteLine("A fatal system error has occured and BlackComb OS has been shut down to prevent damage to your computer.\r\nExeption: ", error, ".\r\nIf you figure out a way to get support give them this info: \r\nSystem version: 0.0.6\r\nRebuild no: 6");
+        }
+    }
+}
---
+++ Code/System/Error/Error.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackcombOS.System.Error
+{
+    internal class Error
+    {
+        public static class Errors
+        {
+
+            public static string SystemError
+            {
+                get
+                {
+                    return "Error! SystemException: Command not found. Error Code: 100";
+                }
+            }
+            public static string IOException
+            {
+                get
+                {
+                    return "Error! IOException: Directory not found";
+                }
+            }
+            public static string IOFilestreamException
+            {
+                get
+                {
+                    return "Error! IOException: File not found";
+                }
+            }
+        }
+    }
+}
